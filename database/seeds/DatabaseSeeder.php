ef990bb2c2fc4dbd80eb28020714c5b446ccc2ce
Ariama Victor (A.K.A. OVAC BOYA) <iamovac@gmail.com>
2018-02-18 09:28:15 +0000

    Database Seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        if (config('app.env') === 'local') {
+            $this->call(UsersTableSeeder::class);
+            $this->call(ResturantTableSeeder::class);
+            $this->call(ItemsTableSeeder::class);
+            $this->call(RatingsTableSeeder::class);
+            $this->call(TransactionsTableSeeder::class);
+            $this->call(PicturesTableSeeder::class);
+        }
     }
 }
