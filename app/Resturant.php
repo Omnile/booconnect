85962575a8deaa68889778d955abc805cc8399d0
Ariama Victor (A.K.A. OVAC BOYA) <iamovac@gmail.com>
2018-02-18 09:38:33 +0000

    Morph Images to resturant

--- app/Resturant.php
+++ app/Resturant.php
@@ -26,4 +26,9 @@ class Resturant extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function pictures()
+    {
+        return $this->morphOne(Picture::class, 'imageable');
+    }
 }
