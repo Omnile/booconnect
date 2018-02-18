3a947ad1ce194c19720102622a53905052eeb816
Ariama Victor (A.K.A. OVAC BOYA) <iamovac@gmail.com>
2018-02-18 09:24:50 +0000

    Add Picture Modules

---
+++ app/Picture.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Picture extends Model
+{
+    protected $types = [
+        'user' => User::class,
+        'staff' => Resturant::class,
+        'item' => Item::class,
+    ];
+
+    public function imageable()
+    {
+        return $this->morphTo();
+    }
+
+    // public function getImageableTypeAttribute($type)
+    // {
+    //     // Illuminate/Database/Eloquent/Model::morphTo checks for null in order
+    //     // to handle eager-loading relationships
+    //     if (!$type) {
+    //         return null;
+    //     }
+
+    //     // to make sure this returns value from the array
+    //     return array_get($this->types, $type, $type);
+    // }
+}
---
+++ database/factories/PictureFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Item;
+use App\Resturant;
+use App\User;
+use Faker\Generator as Faker;
+
+$factory->define(App\Picture::class, function (Faker $faker) {
+    return [
+        'path' => $faker->imageUrl(150, 150, 'food'),
+        'description' => $faker->realText($faker->numberBetween(10, 20)),
+        'imageable_id' => rand(1, 100),
+        'imageable_type' => array_random([
+            Resturant::class,
+            User::class,
+            Item::class,
+        ]),
+    ];
+});
