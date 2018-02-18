7ef792b747cbb76a3ff585c28c35dedfd2e6c293
Ariama Victor (A.K.A. OVAC BOYA) <iamovac@gmail.com>
2018-02-18 07:48:28 +0000

    💰 Add transaction Modules

---
+++ app/Transaction.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    public function item()
+    {
+        return $this->hasMany(Item::class);
+    }
+
+    public function resturants()
+    {
+        return $this->belongsToMany(Resturant::class);
+    }
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2018_02_17_220618_create_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('resturant_id');
+            $table->boolean('debit')->default(true);
+            $table->string('description');
+            $table->integer('item_id');
+            $table->integer('user_id');
+            $table->float('amount');
+            $table->json('data');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
