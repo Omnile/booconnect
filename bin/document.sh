7e27e4056657596cfb31287bf9167ad24a02fb8b
Ariama Victor (A.K.A. OVAC BOYA) <iamovac@gmail.com>
2018-02-27 21:43:13 +0000

    force copy on doc files

--- bin/document.sh
+++ bin/document.sh
@@ -35,10 +35,10 @@ deploy_document(){
 	# Remove all files from the
 	shopt -s extglob;
 	shopt -s dotglob nullglob;
-	rm -rf !*.git;
+	rm -rf !('*.git');
 
 	# copy the generated document folder into the empty github folder
-	mv  -fv ../public/docs/* ./;
+	mv  -rfv ../public/docs/* ./;
 
 	# copy all files from the sami generated folder into the api folder
 	# mv  -v ../build/sami/* ./api/;
