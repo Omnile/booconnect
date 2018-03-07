c0ba22b728f1e4dd29209ce03b00a85e3cf9ca94
Ariama Victor (A.K.A. OVAC BOYA) <iamovac@gmail.com>
2018-03-07 23:45:05 +0000

    Add ratings seeder

--- database/factories/RatingFactory.php
+++ database/factories/RatingFactory.php
@@ -6,7 +6,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Rating::class, function (Faker $faker) {
     return [
-        'rating' => rand(1, 5),
+        'rating' => rand(1, config('booconnect.seed.max')),
         'user_id' => rand(1, config('booconnect.seed.max')),
         'comment' => $faker->sentence(),
         'rateable_id' => rand(1, config('booconnect.seed.max')),
--- database/seeds/RatingsTableSeeder.php
+++ database/seeds/RatingsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Rating;
 use Illuminate\Database\Seeder;
 
 class RatingsTableSeeder extends Seeder
@@ -11,6 +12,6 @@ class RatingsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        factory(Rating::class, config('booconnect.seed.max'))->create();
     }
 }
