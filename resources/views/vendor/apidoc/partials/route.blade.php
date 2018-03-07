08fa6f13d40d95cb925fb56cc0cea4532d0c8d34
Ariama Victor (A.K.A. OVAC BOYA) <iamovac@gmail.com>
2018-03-07 21:23:39 +0000

    Update Documentation to reflect changes

--- resources/views/vendor/apidoc/partials/route.blade.php
+++ resources/views/vendor/apidoc/partials/route.blade.php
@@ -51,15 +51,17 @@ $.ajax(settings).done(function (response) {
 @endif
 
 ### HTTP Request
+
 @foreach($parsedRoute['methods'] as $method)
 `{{$method}} {{$parsedRoute['uri']}}`
 
 @endforeach
 @if(count($parsedRoute['parameters']))
+
 #### Parameters
 
 Parameter | Type | Status | Description
---------- | ------- | ------- | ------- | -----------
+---------|-------|-------|-------|-----------
 @foreach($parsedRoute['parameters'] as $attribute => $parameter)
     {{$attribute}} | {{$parameter['type']}} | @if($parameter['required']) required @else optional @endif | {!! implode(' ',$parameter['description']) !!}
 @endforeach
