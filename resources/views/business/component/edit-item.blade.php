313ebee7416d3b936c613655ffdbc4631520a9e1
Ariama Victor (A.K.A. OVAC BOYA) <iamovac@gmail.com>
2018-03-20 10:13:48 +0000

    Update edit-items view

--- resources/views/business/component/edit-item.blade.php
+++ resources/views/business/component/edit-item.blade.php
@@ -15,43 +15,67 @@
 <div class="container-fluid">
     <!-- Start Page Content -->
     <div class="row">
-                    <div class="col-lg-10 offset-1">
+                    <div class="col-md-3 text-center">
+                    <div class="thumbnail"  style="border: 1px dotted grey; border-radius: 10px; padding: 10px; margin-top: 15px;">
+                      <img src="{{ $item->image }}" alt="{{ $item->name }}" class="img-responsive">
+                      <div class="caption">
+                        <h3>{{ $item->name }}</h3>
+                        <p>Quantity:  {{ $item->quantity }}</p>
+                        <p>Price: {{ $item->price }}</p>
+                      </div>
+                    </div>
+                    <br>
+                  </div>
+
+                    <div class="col-md-9 ">
                         <div class="card">
                             <div class="card-body">
                                 <div class="basic-elements">
-                                    <form>
+                                    <form action="."  enctype="multipart/form-data" method="POST">
+                                        @method('PATCH')
+                                        @csrf
                                         <div class="row">
                                             <div class="col-lg-6">
                                                 <div class="form-group">
                                                     <label>Name</label>
-                                                    <input type="text" class="form-control" placeholder="product name">
+                                                    <input type="text" class="form-control" placeholder="product name" name="name" value="{{ $item->name }}">
                                                 </div>
-                                                
+                                                <div class="form-group">
+                                                    <label>Price ({{ config('booconnect.currency') }})</label>
+                                                    <input type="number" class="form-control" placeholder="product price" name="price" value="{{ $item->price }}">
+                                                </div>
                                                 <div class="form-group">
                                                     <label>Description</label>
-                                                    <textarea class="form-control" rows="8" placeholder="description" style="width: 100%; height: 100%;"></textarea>
+                                                    <textarea class="form-control" rows="8" placeholder="description" style="width: 100%; height: 100%;" name="description">{{ $item->description }}</textarea>
                                                 </div>
-
                                             </div>
                                             <div class="col-lg-6">
-                                                <div class="form-group">
-                                                    <label>Size</label>
-                                                    <select class="form-control">
-						<option>Small</option>
-						<option>Medium</option>
-						<option>Large</option>
-					</select>
-                                                </div>
+
                                                 <div class="form-group">
                                                     <label>Image</label>
-                                                    <input type="file" class="form-control" value="image">
+                                                    <input type="file" class="form-control" value="image" name="image">
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label>Measurement</label>
+                                                    <select class="form-control" name="measurement">
+                                                        <option value="N\A">N\A</option>
+                                                        <option value="plate">Plate</option>
+                                                        <option value="bowl">Bowl</option>
+                                                        <option value="cup">Cup</option>
+                                                        <option value="bottle">Bottle</option>
+                                                    </select>
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label>Quantity daily</label>
+                                                    <input type="number" class="form-control" placeholder="product daily quantity" name="quantity" value="{{ $item->quantity }}">
                                                 </div>
                                             </div>
                                         </div>
-
-                                                <div class="float-right">
-                                                	<button type="submit" class="btn btn-primary">Update Item </button>
-                                                </div>
+                                        <div class="float-right">
+                                            <button type="submit" class="btn btn-primary">Update</button>
+                                        </div>
                                     </form>
                                 </div>
                             </div>
