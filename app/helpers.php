e567c0de0dffad9f342480ddccf01c200de5e0c9
Ariama Victor (A.K.A. OVAC BOYA) <iamovac@gmail.com>
2018-03-26 16:18:12 +0000

    Enable Flash Notification

--- app/helpers.php
+++ app/helpers.php
@@ -6,14 +6,14 @@
  * @param  [String] $message [A message to send to the flash function object]
  * @return  App\Http\Flash@message        [Flash Function from the Flash Class]
  */
-// function flash($title = null, $message = null)
-// {
-//     $flash = app('App\Http\Flash');
-//     if (func_num_args() == 0) {
-//         return $flash;
-//     }
-//     return $flash->info($title, $message);
-// }
+function flash($title = null, $message = null)
+{
+    $flash = app('App\Http\Flash');
+    if (func_num_args() == 0) {
+        return $flash;
+    }
+    return $flash->info($title, $message);
+}
 
 function plan($title = null, $message = null)
 {
